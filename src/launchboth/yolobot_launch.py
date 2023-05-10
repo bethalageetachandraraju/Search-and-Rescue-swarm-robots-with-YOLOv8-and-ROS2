88e4994da6f3585493e69cc35aba90ac47ca8f67
Bethala Geeta Chandra Raju <56234004+bethalageetachandraraju@users.noreply.github.com>
2023-05-10 13:34:55 -0400

    Add files via upload

---
+++ src/launchboth/yolobot_launch.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    pkg_yolobot_gazebo = get_package_share_directory('yolobot_gazebo')
+    pkg_yolobot_description = get_package_share_directory('yolobot_description')
+    pkg_yolobot_control = get_package_share_directory('yolobot_control')
+    pkg_yolobot_recognition = get_package_share_directory('yolobot_recognition')
+
+    joy_node = Node(
+        package = "joy",
+        executable = "joy_node"
+    )
+
+    start_world = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_yolobot_gazebo, 'launch', 'start_world_launch.py'),
+        )
+    )
+
+    spawn_robot_world = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_yolobot_description, 'launch', 'spawn_yolobot_launch.launch.py'),
+        )
+    )     
+
+    spawn_robot_control = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_yolobot_control, 'launch', 'yolobot_control.launch.py'),
+        )
+    )  
+
+    spawn_yolo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_yolobot_recognition, 'launch', 'launch_yolov8.launch.py'),
+        )
+    )  
+    
+    pkg_box_bot_gazebo = get_package_share_directory('box_bot_gazebo')
+    pkg_box_bot_description = get_package_share_directory('box_bot_description')
+
+    
+
+    spawn_robot_world_box = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_box_bot_description, 'launch', 'multi_spawn_robot_launch.py'),
+        )
+    )     
+
+
+    return LaunchDescription([
+        joy_node,
+        start_world,
+        spawn_robot_world,
+        spawn_robot_control,
+        spawn_yolo,spawn_robot_world_box
+    ])
